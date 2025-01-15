dbb2235507bfb9e45ffd9f4ce4a448b0c7ec4b0a
BIT <BIT@BIT1>
2025-01-15 14:07:59 +0900

    filter-proxy 01/15

--- filter-proxy/src/test/java/filter/FilterTest.java
+++ filter-proxy/src/test/java/filter/FilterTest.java
@@ -1,15 +1,45 @@
 package filter;
 
+import config.AppConfig;
 import config.WebConfig;
+import jakarta.servlet.Filter;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes={WebConfig.class})
+@ContextConfiguration(classes={WebConfig.class, AppConfig.class})
 @WebAppConfiguration
 public class FilterTest {
-    private MockMvc mvc;
+    private static MockMvc mvc;
+
+    @BeforeAll
+    public static void setup(WebApplicationContext webApplicationContext) {
+        Filter filter = (Filter) webApplicationContext.getBean("realFilter");
+        mvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilter(new DelegatingFilterProxy(filter),"/*")
+                .build();
+    }
+
+    @Test
+    public void testHello() throws Exception {
+        mvc
+                .perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(cookie().value("RealFilter","Works"))
+                .andExpect(content().string("<h1>Hello World</h1>"))
+                .andDo(print());
+    }
 }
