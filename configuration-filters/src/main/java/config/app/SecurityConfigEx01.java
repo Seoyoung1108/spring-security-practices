c78d510bab62bff01d91478705b7473a3fdbdc5b
BIT <BIT@BIT1>
2025-01-16 13:47:47 +0900

    configuration-filters 01/16

--- configuration-filters/src/main/java/config/app/SecurityConfigEx01.java
+++ configuration-filters/src/main/java/config/app/SecurityConfigEx01.java
@@ -1,31 +1,54 @@
-package config.web;
+package config.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigEx02 {
+public class SecurityConfigEx01 {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return new WebSecurityCustomizer() {
             @Override
             public void customize(WebSecurity web) {
                 web
-                        .ignoring()
-                        .requestMatchers(new AntPathRequestMatcher("/assets/**"));
+                    .ignoring()
+                    .requestMatchers(new AntPathRequestMatcher("/assets/**"));
             }
         };
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	// http.formLogin().loginPage('').successHandler ~~ : 예전 방식
+    	
+    	/*
+    	http
+    	.formLogin(new Customizer<FormLoginConfigurer<HttpSecurity>>() {
+			@Override
+			public void customize(FormLoginConfigurer<HttpSecurity> t) {
+				// TODO Auto-generated method stub
+				
+			}
+    	});
+    	*/
+    	
+    	http.formLogin((formLogin)->{
+    		//formLogin
+    		//.loginPage("/user/login")
+    		//.usernameParameter("email")
+    		//.loginProcessingUrl("/auth");
+    	});
+    	
+    	
         return http.build();
     }
 }
